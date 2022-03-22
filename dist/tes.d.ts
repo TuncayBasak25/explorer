4cfb00b64c96dafed1a3d6cbd0b20ee11856ccd5
Tuncay - Muhammet Ali BASAK - ERKI <tuncay.basak@outlook.com>
2022-03-22 01:59:52 +0100

    Save

--- dist/tes.d.ts
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=tes.d.ts.map
--- index.d.ts
+++
@@ -1,4 +0,0 @@
-import File from "./core/file";
-import Folder from "./core/folder";
-export { File, Folder };
-//# sourceMappingURL=index.d.ts.map
